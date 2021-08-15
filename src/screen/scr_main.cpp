335d1eba3cceaa7f61ab019246f0d0d19126db4b
Danielbyte <danielmasango7@gmail.com>
2021-08-15 22:20:33 +0200

    Exercise 4.6

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -80,6 +80,21 @@ int main()
     // functionality of the screen object.
     // This function is like client code.
 
+    // Exercise 4.6
+    // This class does a lot of things.
+    // Separation of concerns could be used to make the class more understandable.
+    // Member functions that deal with displaying the screen could be implemented
+    // on a class of their own (Presentation layer).
+    // Member functions that deal with placement and or movement of some
+    // characters on the screen could be put on a class of their own (Application/logic layer).
+
+    // Changing the class' interface means changing the behavior and responsibility of the
+    // class. This may lead to some other issues as more of the member functions will now
+    // need to change as well which can result in a lot of bugs especially for long code.
+
+    // The implementation can be easily changed since it only uses the functionality of the
+    // existing class and this is only adding some functionality of the class.
+
     return 0;
 }
 
